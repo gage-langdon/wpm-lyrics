78cbef7f2c9b538da38e349313a44a45dbb41e8b
Gage <glangdon@loanpal.com>
2021-03-02 16:44:15 -0800

    feat: add ga

--- gatsby-config.js
+++ gatsby-config.js
@@ -28,6 +28,14 @@ module.exports = {
       },
     },
     `gatsby-plugin-gatsby-cloud`,
+    {
+      resolve: `gatsby-plugin-google-gtag`,
+      options: {
+        trackingIds: [
+          "G-BG7WE9KVZ7", // Google Analytics
+        ],
+      },
+    },
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.dev/offline
     `gatsby-plugin-offline`,
